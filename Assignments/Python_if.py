452c3ae43ae3f4124f16dcd019b50c75bdc81ea8
Itz-Djin <Christdjin.B@gmail.com>
2022-07-12 16:54:50 -0600

    Practice assignments

---
+++ Assignments/Python_if.py
@@ -0,0 +1,17 @@
+
+
+num1 = 12
+key = False
+
+if num1 == 12:
+    if key:
+        print('num1 is equal to Twelve and they have the key!')
+    else:
+        print('Num1 is equal to Twelve and they Do NOT have the key!')
+elif num1 < 12:
+    print('Num1 is less than Twelve!')
+else:
+    print('num1 is NOT equal to Twelve!')
+
+
+
