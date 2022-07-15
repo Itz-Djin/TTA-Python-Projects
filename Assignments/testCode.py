db339ca8b913a595a33af5f8fa35c5287cfbc553
Itz-Djin <Christdjin.B@gmail.com>
2022-07-15 11:39:52 -0600

    Update testCode.py

--- Assignments/testCode.py
+++ Assignments/testCode.py
@@ -1,15 +1,20 @@
 
 
-mySentence = 'I love the color'
+mySentence = 'loves the color'
 
 color_list = ['red', 'blue', 'green', 'pink', 'teal', 'black', 'orange']
 
-def color_function():
+def color_function(name):
+    lst = []
     for i in color_list:
-        print('{} {}'.format(mySentence, i))
+        msg = '{0} {1} {2}'.format(name, mySentence, i)
+        lst.append(msg)
+    return lst
 
 
 
 
+lst = color_function('John')
+for i in lst:
+    print(i)
 
-color_function()
