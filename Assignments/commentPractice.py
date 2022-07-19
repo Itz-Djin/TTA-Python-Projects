f132776ea9263533f10f66b3cd6dde0d6dd8cc1d
Itz-Djin <Christdjin.B@gmail.com>
2022-07-19 14:32:40 -0600

    created new assingments

---
+++ Assignments/commentPractice.py
@@ -0,0 +1,23 @@
+
+# This is my note.
+
+"""This is my note
+and this is still my note
+and this again is still my note
+"""
+
+##this is a line of code used the shortcut alt + 3
+
+def printMe():
+    """this is my doctsring
+    """
+
+    print("Meeeeeeeeeee")
+
+
+#__doc__ means the docstring under the printMe() function
+print(printMe.__doc__)
+print(printMe())
+
+
+
