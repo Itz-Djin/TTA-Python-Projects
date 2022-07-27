31f87b2b193ea4c1d4df1d2f6d63e04f4e92ac0b
Itz-Djin <Christdjin.B@gmail.com>
2022-07-27 16:13:49 -0600

    Updated assignment

--- Assignments/db_assignment/db_assignment.py
+++ Assignments/db_assignment/db_assignment.py
@@ -3,17 +3,23 @@ import sqlite3
 # also creates the database filelist
 conn = sqlite3.connect('filelist.db')
 cur = conn.cursor()
-
-# specifies while connected to specific db, to create table tbl_txtFiles if it non-existent and creates the primary key as well as columns utilizing string data type
-with conn:
-    cur.execute("CREATE TABLE IF NOT EXISTS tbl_txtFiles ( \
-        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-            File_Title TEXT, \
-            Extension TEXT \
-            )")
-    conn.commit()
-conn.close()
-
-# create example tuple
 fileList = ('information.docx', 'Hello.txt', 'myImage.png', 'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
+for file in fileList:
+    if file.endswith('.txt'):
+# specifies while connected to specific db, to create table tbl_txtFiles if it non-existent and creates the primary key as well as columns utilizing string data type
+        with conn:
+            cur.execute("CREATE TABLE IF NOT EXISTS tbl_txtFiles ( \
+                ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+                    File_Title TEXT, \
+                    Extension TEXT \
+                    )")
+            cur.execute("INSERT INTO tbl_txtFiles(File_Title, Extension) VALUES (?, ?)"
+                        ("Hello", ".txt")
+                        ("World", ".txt"))
+            conn.commit()
+        conn.close()
+
+print(file)
+
+
 
