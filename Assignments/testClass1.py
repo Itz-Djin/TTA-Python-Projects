2fc8cad540959700db9e2b5b183b18adcf3dd539
Itz-Djin <Christdjin.B@gmail.com>
2022-07-27 20:33:17 -0600

    Update testClass1.py

--- Assignments/testClass1.py
+++ Assignments/testClass1.py
@@ -1,5 +1,3 @@
-
-
 # parent class
 class Organism:
     # class attributes
@@ -17,10 +15,60 @@ class Organism:
         return msg
 
 
+# Child class instance
+class Human(Organism):
+    name = 'MacGuyver'
+    species = 'Homosapien'
+    legs = 2
+    arms = 2
+    origin = 'Earth'
+
+    # Method of Human class
+    def ingenuity(self):
+        msg = "\nCreates a deadly weapon using only a paper clip, chewing gum, and a roll of duct tape!"
+        return msg
 
 
+# another child class instance
+class Dog(Organism):
+    name = 'Spot'
+    species = 'Canine'
+    legs = 4
+    arms = 0
+    dna = "Sequence B"
+    origin = "Earth"
 
+    def bite(self):
+        msg = "\nEmits a loud, menacing growl and bites down ferociously on its target!"
+        return msg
 
+# another child class instance
+class Bacterium(Organism):
+    name = 'X'
+    species = 'Bacteria'
+    legs = 0
+    arms = 0 
+    dna = "Sequence C"
+    origin = 'Mars'
+
+    def replication(self):
+        msg = "\nThe cells begin to divide and mulitply into two seperate organisms!"
+        return msg
 
 
 if __name__ == "__main__":
+    #Instantiate the "Human()" object
+    human = Human()
+    print(human.information())
+    print(human.ingenuity())
+
+    #Instantiate the "Dog()" object
+    dog = Dog()
+    print(dog.information())
+    print(dog.bite())
+
+    #Instantiate the "Bacterium()" object
+    bacteria = Bacterium()
+    print(bacteria.information())
+    print(bacteria.replication())
+
