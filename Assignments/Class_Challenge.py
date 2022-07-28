e735aec5a0622591b4d691a349a665eb581dd1ad
Itz-Djin <Christdjin.B@gmail.com>
2022-07-27 21:55:59 -0600

    Create Class_Challenge.py

---
+++ Assignments/Class_Challenge.py
@@ -0,0 +1,23 @@
+#Class Challenge
+from cgitb import small
+
+
+class Computer:
+    def __init__(self, GPU, CPU, M2Drive):
+        self.GPU = GPU
+        self.CPU = CPU
+        self.M2Drive = M2Drive
+
+    def myfunc(self):
+        print("The GPU is a " + self.GPU)
+
+computer1 = Computer("1660TI", "Ryzen", "970 EVO")
+
+computer1.myfunc()
+
+#Inheritance Challenge
+class smallComputer(Computer):
+    pass
+
+microPuter = smallComputer("MiniTI", "MiniRyzen", "Mini EVO")
+microPuter.myfunc()
