bb17eaa0286489a7ab6d5fef1e735a53dc814d32
Itz-Djin <Christdjin.B@gmail.com>
2022-07-27 20:20:09 -0600

    Started class practice

---
+++ Assignments/testTandem-1.py
@@ -0,0 +1,25 @@
+
+
+from tkinter import Variable
+
+
+class Game:
+    variable1 = 'Hello'
+    variable2 = 'World!'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    x = Game()
+    print("{} {}".format(x.variable1, x.variable2))
