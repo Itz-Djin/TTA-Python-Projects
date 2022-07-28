94ebd7e56f1a339d4283d077d9fe6fe1c3f95d6b
Itz-Djin <Christdjin.B@gmail.com>
2022-07-28 16:25:06 -0600

    Update Python_Course_Practicing.py

--- Assignments/Python_Course_Practicing.py
+++ Assignments/Python_Course_Practicing.py
@@ -10,4 +10,4 @@ def myFun(*args):
     for arg in args:
         print(arg)
 
-myFun('This is an example of args', 'string', 'and then an integer', 5)
+myFun('This is an example of args', 'string', 'and then an integer', 5, 'You can add', 'as many arguments', 'up to the max limit')
