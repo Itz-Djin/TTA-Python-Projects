cf803a0aac795f0540b5da8b2cd66084b397d539
Itz-Djin <Christdjin.B@gmail.com>
2022-07-27 19:59:59 -0600

    Completed Inheritance assignment

--- Assignments/inheritance.py
+++ Assignments/inheritance.py
@@ -1,12 +1,14 @@
-
+#Created Electronics class
 class Electronics:
     powered = ''
     model_number = 0
     components = ''
     bluetooth = True
+#Created Screens subclass of Electronices with 2 specific attributes
 class Screens(Electronics):
     size = 0
     shape = ''
+#Created Speakers subclass of Electronics with 2 specific attributes
 class Speakers(Electronics):
     decibals = 0
     variant = ''
