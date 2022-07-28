75d50f48ff61dd0f9a8e60ec1088b284cd6d8e75
Itz-Djin <Christdjin.B@gmail.com>
2022-07-27 19:52:22 -0600

    Update db_assignment.py

--- Assignments/db_assignment/db_assignment.py
+++ Assignments/db_assignment/db_assignment.py
@@ -3,24 +3,38 @@ import sqlite3
 # also creates the database filelist
 conn = sqlite3.connect('filelist.db')
 cur = conn.cursor()
-fileList = ('information.docx', 'Hello.txt', 'myImage.png', 'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
-for file in fileList:
-    if file.endswith('.txt'):
+
 # specifies while connected to specific db, to create table tbl_txtFiles if it non-existent and creates the primary key as well as columns utilizing string data type
+with conn:
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_txtFiles ( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+            File_Title TEXT, \
+            Extension TEXT \
+            )")
+    conn.commit()
+conn.close()
+
+# list of file names
+fileList = ('information.docx', 'Hello.txt', 'myImage.png', 'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
+
+#iterates through the list of file names
+for file in fileList:
+    #if the file name ense with ".txt" then...
+    if file.endswith('.txt'):
+        #re-establishes 'conn' conecction to the database
+        conn = sqlite3.connect('filelist.db')
+
+        #opens connection to database and says what to do with said connection
         with conn:
-            cur.execute("CREATE TABLE IF NOT EXISTS tbl_txtFiles ( \
-                ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-                    File_Title TEXT, \
-                    Extension TEXT \
-                    )")
-            cur.execute("INSERT INTO tbl_txtFiles(File_Title, Extension) VALUES (?, ?)" \
-                        ("Hello", ".txt"))
-            cur.execute("INSERT INTO tbl_txtFiles(File_Title, Extension) VALUES (?, ?)" \
-                        ("World", ".txt"))
+            #executes the SQL statement to insert the 'file' variable into the table
+            cur.execute("INSERT INTO tbl_txtFiles(File_Title) VALUES (?)" \
+                (file,))
+
+            #Displays the name of the file
+            print(file)
+            #commits changes to database
             conn.commit()
+        #closes the connection to the database
         conn.close()
 
-print(file)
-
-
-
+    
