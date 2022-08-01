27dab895fcc8c689bc3bbb1eae43e5865870e670
Itz-Djin <Christdjin.B@gmail.com>
2022-08-01 16:56:38 -0600

    Update Abstraction_Challenge.py

--- Assignments/Abstraction_Challenge.py
+++ Assignments/Abstraction_Challenge.py
@@ -2,9 +2,13 @@ from abc import ABC, abstractmethod
 
 #Created abstract parent class
 class Vehicle(ABC):
-    #defines the abstract class, preventing any objects to be made of this class
+    #defines the abstract class, preventing any objects to be instantiated of this class
     @abstractmethod
     def go(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
         pass
 
 #created child class
@@ -13,11 +17,17 @@ class Car(Vehicle):
     def go(self):
         print("you drive the car")
 
+    def stop(self):
+        print("This car is stopped")
+
 #created child class
 class Motorcycle(Vehicle):
 
     def go(self):
         print("you ride the motorcycle")
+
+    def stop(self):
+        print("This motorcycle is stopped")
 
 #vehicle = Vehicle()
 car = Car()
@@ -26,3 +36,5 @@ motorcycle = Motorcycle()
 #vehicle.go
 car.go()
 motorcycle.go()
+car.stop()
+motorcycle.stop()
